889d083a3d0917533bedb848f9ed971eda94af49
tlacy1000 <tlacy1000@gmail.com>
2019-11-18 15:16:48 -0600

    styling and app complete

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+SELECT * FROM burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES
+("Magic shroom w/ extra pesto"),
+("Mediterannean burger"),
+("Double quarter pounder w/extra cheese"),
+("Impossible burger w/grilled onions + bellpeppers");
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+("Turkey burger w/ honey sriracha", true);
